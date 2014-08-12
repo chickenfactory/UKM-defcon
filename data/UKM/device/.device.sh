8727c707c5a2cb7c19098e2ca94380f7873fed40
Micah <micah.chickenfactory@gmail.com>
2014-08-12 00:59:59 -1000

    initial push

---
+++ data/UKM/device/.device.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+
+UKM=/data/UKM;
+BB=$UKM/busybox;
+DEVICE=`getprop ro.product.device`;
+
+if [ $DEVICE == "mako" ]; then
+	CONFIG="mako"; #Nexus 4
+elif [ $DEVICE == "hammerhead" ]; then
+	CONFIG="hammerhead"; #Nexus 5
+elif [ $DEVICE == "deb" -o $DEVICE == "flo" ]; then
+	CONFIG="deb"; #Nexus 7
+elif [ $DEVICE == "A0001" ]; then
+	CONFIG="A0001"; #OnePlus One
+else
+	CONFIG="mako"; #Not defined: Nexus 4
+fi;
+
+PATH=$UKM/device/$CONFIG.sh;
+
+$BB echo $PATH;
